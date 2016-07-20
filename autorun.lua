7e319ce6ff2755d6e8d27e2b1036f43ff24a22b6
Nexustix <nexus1768@tuta.io>
2016-07-20 19:59:45 +0200

    Change .dwl miniaturization

--- autorun.lua
+++ autorun.lua
@@ -8,13 +8,15 @@ shell.execute("cd /home/nexustix")
 shell.execute("cp /home/nexustix/subs.dwl /home/nexustix/tmp.dwl")
 
 local subs = io.open("/home/nexustix/tmp.dwl", "r")
+local prefix = "https://raw.githubusercontent.com/nexustix/OC/master/"
 
 while true do
 
     local line = subs:read("*l")
 
     if (line ~= nil) then --and (line ~= "") then
-        shell.execute("wget -f "..line)
+        --shell.execute("wget -f "..line)
+        shell.execute("wget -f "..prefix..line)
     else
         break
     end
--- subs.dwl
+++ subs.dwl
@@ -1,4 +1,4 @@
-https://raw.githubusercontent.com/nexustix/OC/master/subs.dwl
-https://raw.githubusercontent.com/nexustix/OC/master/autorun.lua
-https://raw.githubusercontent.com/nexustix/OC/master/geoutil/geoscan.lua
-https://raw.githubusercontent.com/nexustix/OC/master/geoutil/geoutil.lua /lib/geoutil.lua
+subs.dwl
+autorun.lua
+geoutil/geoscan.lua
+geoutil/geoutil.lua /lib/geoutil.lua

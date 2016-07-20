64d9123af5256d5381bd54d540139e791ebe621b
Nexustix <nexus1768@tuta.io>
2016-07-20 13:14:49 +0200

    Add config test

--- geotools/geoscan.lua
+++ geotools/geoscan.lua
@@ -1,4 +1,28 @@
 local component = require("component")
+local slz = require("serialization")
+
+function writeln(src, dest)
+    dest:write(src)
+    dest:write("\n")
+end
+
+function addValue(val, dest)
+    local slzVar = slz.serialize(val)
+    writeln(slzVar, dest)
+end
+
+local configpath = "geo.cfg"
+local stdConfig = {}
+
+stdConfig["cake"] = "lie"
+
+if false then
+    file = io.open(configpath,"w")
+    addValue(stdConfig)
+    file:close()
+end
+
+file = io.open(configpath, "r")
 
 local geo = component.geolyzer
 

4419515a2e5f01b6981fcbfd96ecc4cab656090e
Nexustix <nexus1768@tuta.io>
2016-07-21 18:31:58 +0200

    Add send.lua

---
+++ CC/send.lua
@@ -0,0 +1,40 @@
+local msg = "the cake is a lie"
+
+function flush()
+    redstone.setOutput("back", 1)
+    redstone.setOutput("back", 0)
+end
+
+function sendTwoBits(bitZero, bitOne)
+    if bitZero then
+        redstone.setOutput("left", 1)
+    else
+        redstone.setOutput("left", 0)
+    end
+
+    if bitOne then
+        redstone.setOutput("right", 1)
+    else
+        redstone.setOutput("right", 0)
+    end
+
+    flush()
+end
+
+function charToBinary(msgChar)
+    local remain = msgChar
+    local translation = {}
+    for i = 7, 0, -1 do
+        if (remain >= curNum) then
+            translation[i] = 1
+        else
+            translation[i] = 0
+        end
+    end
+    return translation
+end
+
+for k, v in pairs(translation) do
+    io.write(v)
+end
+print()

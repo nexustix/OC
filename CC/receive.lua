e359df384a82ade2c0a9aee7f885ec36ae82a36e
Nexustix <nexus1768@tuta.io>
2016-07-21 20:08:12 +0200

    Fix only accept on signal change

--- CC/receive.lua
+++ CC/receive.lua
@@ -1,9 +1,13 @@
 local buffer = {}
 local index = 0
+local curBack = false
+local lastBack = false
 
 while true do
     local event = os.pullEvent("redstone")
-    if redstone.getInput("back") then
+    curBack =redstone.getInput("back")
+    if (curBack) and (curBack ~= lastBack) then
+        lastBack = true
         buffer[index] = redstone.getInput("right")
         buffer[index+1] = redstone.getInput("left")
         index = index + 2
@@ -13,7 +17,8 @@ while true do
             io.write(tostring(v))
         end
         print()
-    else
+    elseif not curBack then
+        lastBack = false
         redstone.setOutput("front", false)
     end
 end

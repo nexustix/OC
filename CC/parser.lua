3411b4ab6a6041aff7ed3429910f58cb6373861f
Nexustix <nexus1768@tuta.io>
2016-07-22 16:23:51 +0200

    Add clear screen on special characters

--- CC/parser.lua
+++ CC/parser.lua
@@ -1,7 +1,16 @@
 rednet.open("top")
 shell.run("clear")
+
+local buffer = io.open("buffer.buff", "w")
+
 while true do
-  event, senderId, message, protocol = os.pullEvent("rednet_message")
-  --print( "Computer: "..senderId.." sent a message: "..message.." using "..protocol )
-  io.write(string.char(message))
+    event, senderId, message, protocol = os.pullEvent("rednet_message")
+    --print( "Computer: "..senderId.." sent a message: "..message.." using "..protocol )
+    if (message >= 32) then
+        io.write(string.char(message))
+    else
+        shell.run("clear")
+    end
+
+    buffer:write(string.char(message))
 end

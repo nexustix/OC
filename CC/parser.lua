874bf25fec50dab166743a1d71b6b0d25a4dcecb
Nexustix <nexus1768@tuta.io>
2016-07-22 18:30:49 +0200

    Add display output on monitor

--- CC/parser.lua
+++ CC/parser.lua
@@ -2,12 +2,14 @@ rednet.open("top")
 shell.run("clear")
 
 local buffer = io.open("buffer.buff", "w")
+local monitor = peripheral.wrap( "bottom" )
 
 while true do
     event, senderId, message, protocol = os.pullEvent("rednet_message")
     --print( "Computer: "..senderId.." sent a message: "..message.." using "..protocol )
     --if (message >= 32) then
         io.write(string.char(message))
+        monitor.write(string.char(message))
         --print("<-> "..message.." >"..string.char(message).."<")
     --else
         --shell.run("clear")

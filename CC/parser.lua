477d9d7cde6523b1a0a67e27e1784a1ee91d6946
Nexustix <nexus1768@tuta.io>
2016-07-22 15:57:24 +0200

    Add write message to stdout

--- CC/parser.lua
+++ CC/parser.lua
@@ -1,5 +1,7 @@
 rednet.open("top")
+shell.run("clear")
 while true do
   event, senderId, message, protocol = os.pullEvent("rednet_message")
-  print( "Computer: "..senderId.." sent a message: "..message.." using "..protocol )
+  --print( "Computer: "..senderId.." sent a message: "..message.." using "..protocol )
+  io.write(string.char(message))
 end

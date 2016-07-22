9a08b8bcf616e681b51f823c1e1ab8185e95b664
Nexustix <nexus1768@tuta.io>
2016-07-22 23:05:33 +0200

    Remove writing to monitor

--- CC/parser.lua
+++ CC/parser.lua
@@ -2,9 +2,9 @@ rednet.open("top")
 shell.run("clear")
 
 local buffer = io.open("buffer.buff", "w")
-local monitor = peripheral.wrap( "bottom" )
-monitor.clear()
-term.setCursorPos(1,1)
+--local monitor = peripheral.wrap( "bottom" )
+--monitor.clear()
+--term.setCursorPos(1,1)
 
 
 while true do
@@ -12,7 +12,7 @@ while true do
     --print( "Computer: "..senderId.." sent a message: "..message.." using "..protocol )
     --if (message >= 32) then
         io.write(string.char(message))
-        monitor.write(string.char(message))
+        --monitor.write(string.char(message))
         --print("<-> "..message.." >"..string.char(message).."<")
     --else
         --shell.run("clear")

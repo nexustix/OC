3af511a9db62a6d709df8e419ea378448885d950
Nexustix <nexus1768@tuta.io>
2016-07-22 16:36:46 +0200

    Change print all characters

--- CC/parser.lua
+++ CC/parser.lua
@@ -6,11 +6,12 @@ local buffer = io.open("buffer.buff", "w")
 while true do
     event, senderId, message, protocol = os.pullEvent("rednet_message")
     --print( "Computer: "..senderId.." sent a message: "..message.." using "..protocol )
-    if (message >= 32) then
-        io.write(string.char(message))
-    else
-        shell.run("clear")
-    end
+    --if (message >= 32) then
+        --io.write(string.char(message))
+        print("<-> "..message.." >"..string.char(message).."<")
+    --else
+        --shell.run("clear")
+    --end
 
     buffer:write(string.char(message))
 end

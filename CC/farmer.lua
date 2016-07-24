c2565e8cd4dcb867fa54f3fba001e3e92dfb1e04
Nexustix <nexus1768@tuta.io>
2016-07-24 13:59:10 +0200

    Add implement harvesting

--- CC/farmer.lua
+++ CC/farmer.lua
@@ -9,8 +9,11 @@ while true do
     function farm()
         local success_1, data_1 = turtle.inspectDown()
         if success_1 then
-          print("D Block name: ", data_1.name)
-          print("D Block metadata: ", data_1.metadata)
+            if data_1.metadata >= 7 then
+                turtle.digDown()
+            end
+          --print("D Block name: ", data_1.name)
+          --print("D Block metadata: ", data_1.metadata)
         end
     end
 
@@ -21,4 +24,6 @@ while true do
         turtle.forward()
     else
     end
+
+    os.sleep(0.25)
 end

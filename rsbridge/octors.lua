e0d838130e943e41994dd6c71bb95b6299cbf332
Nexustix <nexus1768@tuta.io>
2016-07-24 22:03:53 +0200

    Change start removing OpenOS dependency

--- rsbridge/octors.lua
+++ rsbridge/octors.lua
@@ -2,13 +2,22 @@ local curFront = 0
 local lastFront = 0
 local dbg = false
 
-local component = require("component")
-local redstone = component.redstone
+--local component = require("component")
+--local redstone = component.redstone
+local redstone = component.proxy(component.list("redstone")())
 
-local event = require("event")
-local sides = require("sides")
+--local event = require("event")
+--local sides = require("sides")
 
-local modem = component.modem
+local sides = {}
+
+sides.left = 5
+sides.right = 4
+sides.front = 3
+sides.back = 2
+
+--local modem = component.modem
+local modem = component.proxy(component.list("modem")())
 
 modem.open(3587)
 
@@ -42,7 +51,8 @@ function flush()
 
         lastFront = curFront
         --local event = os.pullEvent("redstone")
-        local event = event.pull(nil, "redstone")
+        --local event = event.pull(nil, "redstone")
+        local event = computer.pullSignal(nil, "redstone")
     end
 end
 
@@ -102,50 +112,8 @@ function sendString(message)
     end
 end
 
-function sendFile(filepath)
-    --local tmpFile = io.open(filepath, "r")
-    local lineNumber = 1
-    for line in io.lines(filepath) do
-        debug("line "..tostring(lineNumber))
-        sendString(line.."\n")
-        lineNumber = lineNumber + 1
-    end
-
-end
-
---local charTest = false
---local messageTest = true
---local fileTest = false
---local tmpChar = 121
-
 while true do
-    local _, _, from, port, _, message = event.pull("modem_message")
+    --local _, _, from, port, _, message = event.pull("modem_message")
+    local _, _, from, port, _, message = computer.pullSignal(nil, "modem_message")
     sendString(tostring(message))
 end
-
---[[
-if charTest then
-    for k, v in pairs(charToBinary(tmpChar)) do
-        if v then
-            --io.write("1")
-            print(k.." ".."1")
-        else
-            --io.write("0")
-            print(k.." ".."0")
-        end
-    end
-    print()
-    sendChar(tmpChar)
-elseif messageTest then
-    sendString("this is a test")
-
-elseif fileTest then
-    sendFile("test")
-else
-    sendTwoBits(true, true)
-    sendTwoBits(false, false)
-    sendTwoBits(false, true)
-    sendTwoBits(true, false)
-end
-
---]]--
